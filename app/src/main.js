f7ba87bb610e9eef43e5eb4131185fd9422c5092
Ajene <ajenec@gmail.com>
2024-12-05 12:00:46 -0500

    aj commit

---
+++ app/src/main.js
@@ -0,0 +1,11 @@
+//import "./style.css";
+import palettes from "./palettes.json" assert { type: "json" };
+import { v4 as generateUUID } from "uuid";
+
+const newPaletteID = generateUUID();
+
+setPalettes(newPalettes);
+getPalettes();
+initPalettesIfEmpty();
+addPalette(newPalette);
+removePalette(paletteUuid);
